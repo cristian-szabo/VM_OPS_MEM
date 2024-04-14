eb66b6801ed72c0487d98fd6594ca704613d4ad4
Cristian Szabo <cristian.szabo@users.noreply.github.com>
2024-04-14 09:04:18 +0000

    fix x86 compiler errors

--- VmOpsMem/ops_x86_64.cpp
+++ VmOpsMem/ops_x86_64.cpp
@@ -58,9 +58,47 @@ struct tile_config_t {
     uint8_t rows[16];
 };
 
+struct Features {
+    union {
+        struct {
+            unsigned int Stepping : 4 - 0, Model : 8 - 4, Family : 12 - 8, ProcType : 14 - 12,
+                Reserved1 : 16 - 14, ExtModel : 20 - 16, ExtFamily : 28 - 20, Reserved2 : 32 - 28;
+        } EAX;
+        unsigned int Signature;
+    };
+    struct {
+        unsigned int Brand_ID : 8 - 0, CLFSH_Size : 16 - 8, Max_SMT_ID : 24 - 16,
+            Init_APIC_ID : 32 - 24;
+    } EBX;
+    struct {
+        unsigned int SSE3 : 1 - 0, /* AMD Family 0Fh		*/
+            PCLMULDQ : 2 - 1, DTES64 : 3 - 2, MONITOR : 4 - 3, DS_CPL : 5 - 4, VMX : 6 - 5,
+            SMX : 7 - 6, EIST : 8 - 7, TM2 : 9 - 8, SSSE3 : 10 - 9, /* AMD Family 0Fh		*/
+            CNXT_ID : 11 - 10, SDBG : 12 - 11, /* IA32_DEBUG_INTERFACE MSR support */
+            FMA : 13 - 12, CMPXCHG16 : 14 - 13, xTPR : 15 - 14, PDCM : 16 - 15, Reserved : 17 - 16,
+            PCID : 18 - 17, DCA : 19 - 18, SSE41 : 20 - 19, SSE42 : 21 - 20,
+            x2APIC : 22 - 21, /* x2APIC capability		*/
+            MOVBE : 23 - 22, POPCNT : 24 - 23, TSC_DEADLINE : 25 - 24, AES : 26 - 25,
+            XSAVE : 27 - 26, OSXSAVE : 28 - 27, AVX : 29 - 28, F16C : 30 - 29, RDRAND : 31 - 30,
+            Hyperv : 32 - 31; /* This bit is set by the Hypervisor */
+    } ECX;
+    struct { /* Most common x86					*/
+        unsigned int FPU : 1 - 0, VME : 2 - 1, DE : 3 - 2, PSE : 4 - 3, TSC : 5 - 4, MSR : 6 - 5,
+            PAE : 7 - 6, MCE : 8 - 7, CMPXCHG8 : 9 - 8, APIC : 10 - 9, Reserved1 : 11 - 10,
+            SEP : 12 - 11, MTRR : 13 - 12, PGE : 14 - 13, MCA : 15 - 14, CMOV : 16 - 15,
+            PAT : 17 - 16, PSE36 : 18 - 17, PSN : 19 - 18, /* Intel Processor Serial Number */
+            CLFLUSH : 20 - 19, Reserved2 : 21 - 20, DS_PEBS : 22 - 21, ACPI : 23 - 22,
+            MMX : 24 - 23, FXSR : 25 - 24,               /* FXSAVE and FXRSTOR instructions. */
+            SSE : 26 - 25, SSE2 : 27 - 26, SS : 28 - 27, /* Intel			*/
+            HTT : 29 - 28, TM1 : 30 - 29,                /* Intel			*/
+            Reserved3 : 31 - 30, PBE : 32 - 31;          /* Intel			*/
+    } EDX;
+};
+
 std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
 
-static std::vector<LogicalCore> processors;
+std::vector<LogicalCore> processors;
+Features features;
 
 inline unsigned short
 FindMaskWidth(unsigned short maxCount) {
@@ -70,6 +108,23 @@ FindMaskWidth(unsigned short maxCount) {
         maskWidth++;
 
     return maskWidth;
+}
+
+static void
+QueryFeatures() {
+    __asm__ volatile("movq	$0x1,  %%rax	\n\t"
+                     "xorq	%%rbx, %%rbx	\n\t"
+                     "xorq	%%rcx, %%rcx	\n\t"
+                     "xorq	%%rdx, %%rdx	\n\t"
+                     "cpuid			\n\t"
+                     "mov	%%eax, %0	\n\t"
+                     "mov	%%ebx, %1	\n\t"
+                     "mov	%%ecx, %2	\n\t"
+                     "mov	%%edx, %3"
+                     : "=r"(features.EAX), "=r"(features.EBX), "=r"(features.ECX),
+                       "=r"(features.EDX)
+                     :
+                     : "%rax", "%rbx", "%rcx", "%rdx");
 }
 
 static void
@@ -107,7 +162,7 @@ MapCpuTopology(LogicalCore &core) {
                      :
                      : "%rax", "%rbx", "%rcx", "%rdx");
 
-    if (Features.Std.EDX.HTT) {
+    if (features.EDX.HTT) {
         SMT_Mask_Width = leaf1_ebx.Max_SMT_ID;
 
         __asm__ volatile("movq	$0x4,  %%rax	\n\t"
@@ -158,6 +213,9 @@ init() {
         return true;
     }
 
+    unsigned OSProcessorCount = sysconf(_SC_NPROCESSORS_CONF);
+    processors.resize(OSProcessorCount);
+
     for (unsigned i = 0; i < OSProcessorCount; ++i) {
         set_thread_affinity(i);
         std::this_thread::yield();
@@ -182,8 +240,8 @@ cpu_time() {
     auto elapsed_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
 
     CpuResult r;
-    r.cycles = ((uint64_t) hi << 32) | lo;
-    r.time = elapsed_time.count();
+    r.Cycles = ((uint64_t) hi << 32) | lo;
+    r.Time = elapsed_time.count();
     return r;
 }
 
@@ -259,7 +317,7 @@ amx_s8_s32(uint64_t steps) {
     uint64_t ops = steps * ops_per_output * 16 * 16 /* num outputs */;
 
     auto r = Result{duration.count(), ops};
-    std::memcpy(r.output, res, sizeof(res));
+    std::memcpy(r.Output, res, sizeof(res));
     return r;
 };
 #endif
@@ -302,7 +360,7 @@ amx_bf16_f32(uint64_t steps) {
     uint64_t ops = steps * ops_per_output * 16 * 16 /* num outputs */;
 
     auto r = Result{duration.count(), ops};
-    std::memcpy(r.output, res, sizeof(res));
+    std::memcpy(r.Output, res, sizeof(res));
     return r;
 };
 #endif
@@ -336,7 +394,7 @@ vnn_s8_s32(uint64_t steps) {
     uint64_t ops = steps * ops_per_inst /* num outputs */;
 
     auto r = Result{duration.count(), ops};
-    std::memcpy(r.output, &res, sizeof(res));
+    std::memcpy(r.Output, &res, sizeof(res));
     return r;
 };
 #endif
@@ -370,7 +428,7 @@ vnn_f16_f32(uint64_t steps) {
     uint64_t ops = steps * ops_per_inst /* num outputs */;
 
     auto r = Result{duration.count(), ops};
-    std::memcpy(r.output, &res, sizeof(res));
+    std::memcpy(r.Output, &res, sizeof(res));
     return r;
 };
 #endif
